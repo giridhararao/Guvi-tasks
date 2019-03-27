e83f12cc6dc07eb7204e5fce6e133de6b2eac5d7
M.Giridhara Rao <www.giridhararao@gmail.com>
2019-03-27 06:22:15 +0530

    Create resource.java

---
+++ FurnitureMagik/project/resource.java
@@ -0,0 +1,30 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import org.json.JSONObject;
+
+
+@path('/mainpath')
+public class ResoucePath{
+      @GET
+    @Path("/alldata")
+    @Produces({MediaType.APPLICATION_JSON})
+    public ArrayList<DataModel> getOutletStatus(@Context HttpServletRequest req, @QueryParam("category1") String category1,@QueryParam("category2") String category2) {
+      return Dao.getalldata(category1,category2);
+    }
+
+    @GET
+    @Path("/categories")
+    @Produces({MediaType.APPLICATION_JSON})
+    public ArrayList<String> getArea(@Context HttpServletRequest req) {
+      return Dao.getCategories();
+
+}
+
