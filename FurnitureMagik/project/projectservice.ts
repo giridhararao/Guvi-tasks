a9a6f68cbf2b5471492e5f42612cfcc01939c03d
M.Giridhara Rao <www.giridhararao@gmail.com>
2019-03-27 06:31:35 +0530

    Create projectservice.ts

---
+++ FurnitureMagik/project/projectservice.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import {Observable} from "rxjs/index";
+import { alldata } from './projectdata';
+@Injectable()
+
+export class projectservice {
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getalldata(selectedcate1: string,selectedcate1:string) {
+    let _params: HttpParams = new HttpParams();
+    _params = _params.append('category1', "" + selectedcate1);
+    _params = _params.append('category2', ""+selectedcate1);
+    return this.http.get('/mainpath/alldata', {params: _params});
+  }
+  public getcategories(){
+    return this.http.get('/mainpath/categories');
+  }
+
+}
+
+
